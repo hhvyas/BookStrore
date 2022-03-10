98739402239dda6e2ca178180f58e4d8457432a9
hhvyas <hardikvyas1999@gmail.com>
2022-03-11 01:47:37 +0530

    Back-Button added

--- src/Components/BookInfo/BookPreview/BookPreview.jsx
+++ src/Components/BookInfo/BookPreview/BookPreview.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import BookDetails from './BookDetails'
 import BookImageLarge from './BookImageLarge'
 import '../css/BookPreview/BookPreview.css'
+import {Link} from 'react-router-dom'
+import { IoIosArrowBack } from "react-icons/io";
+
 
 function BookPreview() {
   return (
+    <>
+        <Link to='/' className='back-button-link' ><IoIosArrowBack className='back-button-icon'/></Link>
     <div className='book-preview'>
         <BookImageLarge />
         <BookDetails/>
     </div>
+    </>
   )
 }
 
